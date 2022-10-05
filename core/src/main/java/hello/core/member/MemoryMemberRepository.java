e6cb865d17d5e7b0e6f32fe54be3e013f302f736
noak.ku <hozza94@gmail.com>
2022-10-05 16:48:40 +0900

    create MemberApp

--- core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
+    // 동시성 이슈 때문에 ConcurrnetHashMap 을 사용해야 함
 
     @Override
     public void save(Member member) {
